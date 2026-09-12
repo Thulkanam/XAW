52b9df12de74222a5fcf815ab648dedb43f7248e
Cole Thulkanam <ST10503434@myemeris.edu.za>
2026-09-12 06:44:50 +0200

    here
    here

--- pawsitive/App.tsx
+++ pawsitive/App.tsx
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./Sreens/Home";
+import About from "./Sreens/About";
 export default function App() {
   return (
     <View style={styles.container}>
@@ -16,7 +19,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-   //Hello 
-   //lion
   },
 });
